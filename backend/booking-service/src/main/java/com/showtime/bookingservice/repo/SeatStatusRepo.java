5e3f8ff76e00ae80484c3979af76592ca0f1ab3b
Monica-027 <monicaprajapati02@gmail.com>
2025-12-15 17:43:30 +0530

    done locked seat

--- backend/booking-service/src/main/java/com/showtime/bookingservice/repo/SeatStatusRepo.java
+++ backend/booking-service/src/main/java/com/showtime/bookingservice/repo/SeatStatusRepo.java
@@ -1,31 +1,37 @@
 package com.showtime.bookingservice.repo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.showtime.bookingservice.model.Seat;
 import com.showtime.bookingservice.model.SeatStatus;
-import com.showtime.bookingservice.model.User;
-
-import jakarta.persistence.LockModeType;
+import com.showtime.bookingservice.model.Show;
 
 public interface SeatStatusRepo extends JpaRepository<SeatStatus, Integer> {
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT s FROM SeatStatus s WHERE s.show.id = :showId AND s.seat.seatId IN :seatIds")
-    List<SeatStatus> findLockedSeats(@Param("showId") Integer showId,
-                                     @Param("seatIds") List<Integer> seatIds);
 
-	 @Query("SELECT s FROM SeatStatus s WHERE s.seat.seatId = :seatId AND s.show.show_id = :showId")
-	    Optional<SeatStatus> findBySeatAndShow(
-	            @Param("seatId") Integer seatId,
-	            @Param("showId") Integer showId
-	    );
+    //  Find seat status for a seat + show
+    @Query("""
+        SELECT s FROM SeatStatus s
+        WHERE s.seat = :seat
+        AND s.show = :show
+    """)
+    Optional<SeatStatus> findBySeatAndShow(
+            @Param("seat") Seat seat,
+            @Param("show") Show show
+    );
 
-	
-	 
-	
+    //  Used by scheduler to unlock expired seats
+    @Query("""
+            SELECT s FROM SeatStatus s
+            WHERE s.status = 'LOCKED'
+            AND s.Locked_until < :now
+        """)
+        List<SeatStatus> findExpiredLockedSeats(
+                @Param("now") LocalDateTime now
+        );
 }
